3403ef0a8e2a22c26c07ee39fb0e892b6bde7a93
O'meid <public@omeid.me>
2015-09-03 16:32:17 +1000

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+#CSS [![GoDoc](https://img.shields.io/badge/godoc-reference-blue.svg?style=flat-square)](https://godoc.org/github.com/go-gonzo/css)
+Package `css` provides a Gonzo stage for [tdewollf.minify](https://github.com/tdewolff/minify) to Minify CSS files.
---
+++ css.go
@@ -0,0 +1,48 @@
+package css
+
+// github.com/yosssi/gcss binding for gonzo.
+// No Configuration required.
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+
+	"github.com/omeid/gonzo"
+	"github.com/omeid/gonzo/context"
+
+	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/css"
+)
+
+func Minify() gonzo.Stage {
+	return func(ctx context.Context, in <-chan gonzo.File, out chan<- gonzo.File) error {
+
+		for {
+			select {
+			case file, ok := <-in:
+				if !ok {
+					return nil
+				}
+
+				buff := new(bytes.Buffer)
+				name := strings.TrimSuffix(file.FileInfo().Name(), ".css") + ".min.css"
+				ctx.Infof("Compiling %s to %s", file.FileInfo().Name(), name)
+				m := minify.New()
+				m.AddFunc("text/css", css.Minify)
+				err := m.Minify("text/css", buff, file)
+				if err != nil {
+					return err
+				}
+
+				file = gonzo.NewFile(ioutil.NopCloser(buff), file.FileInfo())
+				file.FileInfo().SetSize(int64(buff.Len()))
+				file.FileInfo().SetName(name)
+
+				out <- file
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+		}
+	}
+}
